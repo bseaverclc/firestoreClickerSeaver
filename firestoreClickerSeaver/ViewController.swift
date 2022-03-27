b7c1281642791c85b396dd477ade6370c656c11d
Brian Seaver <bseaver@d155.org>
2022-03-27 18:26:10 -0500

    Initial Commit

---
+++ firestoreClickerSeaver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firestoreClickerSeaver
+//
+//  Created by Brian Seaver on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
